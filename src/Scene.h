84aaab6ab386fd0a1ea01c584d6b46ae870ed5fe
充彦 増田 <torakey0707@gmail.com>
2019-04-23 17:15:11 +0900

    シーンクラスの定義を作成.

--- src/Scene.h
+++ src/Scene.h
@@ -1,1 +1,43 @@
-#pragma once
+#ifndef SCENE_H_INCLUDE
+#define SCENE_H_INCLUDE
+#include <memory>
+#include <string>
+
+class SceneStack;
+
+/*
+* シーンの基底クラス
+*/
+class Scene
+{
+public:
+	Scene(const char* name);
+	Scene(const Scene&) = delete;
+	Scene& operator=(const Scene&) = delete;
+	virtual ~Scene();
+
+	virtual bool Initialize() = 0 {}
+	virtual void ProcessInput() = 0 {}
+	virtual void Update(float) = 0 {}
+	virtual void Render() = 0 {}
+	virtual void Finalize() = 0 {}
+
+	virtual void Play();
+	virtual void Stop();
+	virtual void Show();
+	virtual void Hide();
+
+	const std::string& Name() const;
+	bool IsActive() const;
+	bool IsVisible() const;
+
+private:
+	std::string name;
+	bool isVisible = true;
+	bool isActive = true;
+
+};
+
+using ScenePtr = std::shared_ptr<Scene>;
+
+#endif //SCENE_H_INCLUDE
